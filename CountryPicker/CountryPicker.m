32c3bf590cc0ebdf4e3d74f177180e573e71c5f6
Sergey Guselnikov <arietis@users.noreply.github.com>
2015-10-07 01:04:20 +0300

    Update CountryPicker.m

--- CountryPicker/CountryPicker.m
+++ CountryPicker/CountryPicker.m
@@ -240,7 +240,8 @@
 
     ((UILabel *)[view viewWithTag:1]).text = [[self class] countryNames][(NSUInteger)row];
     NSString *imagePath = [NSString stringWithFormat:@"CountryPicker.bundle/%@", [[self class] countryCodes][(NSUInteger) row]];
-    ((UIImageView *)[view viewWithTag:2]).image = [UIImage imageNamed:imagePath];
+    NSString *path = [[NSBundle bundleForClass:[CountryPicker class]] pathForResource:imagePath ofType:@"png"];
+    ((UIImageView *)[view viewWithTag:2]).image = [UIImage imageWithContentsOfFile:path];
 
 
     return view;
